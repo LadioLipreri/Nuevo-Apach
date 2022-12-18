6ffb637259ce432fadfd049f99219005cf9353a7
LadioLipreri <ladio_lipreri@hotmail.com>
2022-12-17 23:31:46 -0300

    2do commit de la parte del login

--- apach/myapp/views.py
+++ apach/myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import veterinarios, tareas
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 def index(request):
     return render(request, 'index.html')
 
@@ -15,9 +17,21 @@ def About(request):
     return HttpResponse("<h1>About</h1>")
 
 def Registrarse(request):
-    return render(request, "signup.html",{
-        'form':UserCreationForm
+    if request.method == 'GET':#Cuando se envia el formulario
+        return render(request, "signup.html",{
+            'form':UserCreationForm
     })
+    else:#Cuando recibe los datos
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse("Has sido registrado correctamente")
+            except:
+                return HttpResponse("El usuario ya existe")
+        print('Obteniendo datos')
+
+
 
 def Veterinary(request):#el nombre de la vista y del modelo no deben ser iguales
     #misveterinarios = list(veterinarios.objects.values())
