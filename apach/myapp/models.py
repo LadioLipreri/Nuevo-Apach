ccd50137db1a6107251d28aa510e255658064014
LadioLipreri <ladio_lipreri@hotmail.com>
2022-12-20 18:20:36 -0300

    Intentando hacer la parte de noticias

--- apach/myapp/models.py
+++ apach/myapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
 class veterinarios(models.Model):
@@ -18,3 +20,35 @@ class tareas(models.Model):
 
     def __str__(self):
         return self.titulo + ' - ' + self.descripcion + " - " + self.lugar + " - " + str(self.fecha) + " - " + self.responsable.nombre + " - " + self.responsable.apellido
+
+
+#lo del profe
+class Usuario(AbstractUser):
+	pass
+
+class Categoria(models.Model):
+	nombre = models.CharField(max_length = 60)
+
+	def __str__(self):
+		return self.nombre
+
+class Noticia(models.Model):
+
+	titulo = models.CharField(max_length = 150)
+	cuerpo = models.TextField()
+	imagen = models.ImageField(upload_to = 'noticias')
+	categoria_noticia = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+	fecha = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.titulo
+
+class Comentario(models.Model):
+	usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+	texto = models.TextField(max_length = 1500)
+	noticia = models.ForeignKey(Noticia, on_delete = models.CASCADE)
+	fecha = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{noticia}->{texto}"
+
