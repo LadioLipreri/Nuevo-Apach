6e80d643815982b8e5e5d69a562daf0d5366c190
LadioLipreri <ladio_lipreri@hotmail.com>
2022-12-20 19:41:02 -0300

    Registré las tablas de Categoria y Noticia en admin.py

--- apach/myapp/admin.py
+++ apach/myapp/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import veterinarios, tareas
+from .models import veterinarios, tareas, Categoria, Noticia
 # Register your models here.
 
 admin.site.register(veterinarios)
 
 admin.site.register(tareas)
+
+admin.site.register(Categoria)
+
+admin.site.register(Noticia)
