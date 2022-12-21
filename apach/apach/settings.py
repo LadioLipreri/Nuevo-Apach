a9d3bca0cdfb2074f566b9c8f10c17424a9c10ed
LadioLipreri <ladio_lipreri@hotmail.com>
2022-12-21 20:53:01 -0300

    Agregué el archivo requirements.txt

--- apach/apach/settings.py
+++ apach/apach/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'apach.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'apach_store',
+        'USER': 'postgres',
+        'PASSWORD': 'ladiopost12',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
